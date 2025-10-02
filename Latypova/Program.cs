2e146adac13eff58267b196d73005ded296f58c2
Ilgam <ilgamiltinbaev91734@gmail.com>
2025-10-02 20:25:49 +0300

    Добавьте файлы проекта.

---
+++ Latypova/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Security.Cryptography;
+using Latypova;
+namespace Latypova
+{
+    class Programm
+    {
+        static void Main()
+        {/*
+            Console.WriteLine("\tУпражнение 1");
+            int[] num_list = { 1, 2, 3, 4, 5, 9, 7, 8, 9, 10 };
+            bool flag = true;
+            int index = 0;
+            for (int i = 0; i < num_list.Length-1; i++)
+            {
+                if (num_list[i] > num_list[i+1])
+                {
+                    flag = false;
+                    index = i;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                Console.WriteLine("Последовательность возрастающая");
+            }
+            else
+            {
+                Console.WriteLine("Последовательность не возрастающая");
+                Console.WriteLine("Индекс числа: " + index);
+            }
+
+            Console.WriteLine("\tУпражнеине 2");
+            Console.WriteLine("Введите число карты:");
+            string str_mast = Console.ReadLine();
+            if (Int32.TryParse(str_mast, out int int_mast))
+            {
+                try
+                {
+                    string[] cards = { "Шестёрка", "Семёрка", "Восьмёрка", "Девятка", "Десятка", "Валет", "Дама", "Король", "Туз" };
+                    Console.WriteLine("Данная карта номинала:" + cards[int_mast-6]);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Ошибка:"+ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Конец программы");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введены некорректные данные");
+            }
+
+            Console.WriteLine("\tУпражнение 3");
+            Console.Write("Введите данные:");
+            string drinker = Console.ReadLine().ToLower();
+            string drink = "Beer";
+            switch (drinker)
+            {
+                case "jabroni":
+                    drink = "Patron Tequila";
+                    break;
+                case "school counselor":
+                    drink = "Anything with Alcohol";
+                    break;
+                case "programmer":
+                    drink = "Hipster Craft Beer";
+                    break;
+                case "bike gang member":
+                    drink = "Moonshine";
+                    break;
+                case "politician":
+                    drink = "Your tax dollars";
+                    break;
+                case "rapper":
+                    drink = "Cristal";
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(drink);
+            
+            Console.WriteLine("\tУпражнение 4");
+            Console.WriteLine("Введите число недели:");
+            string str_of_week = Console.ReadLine();
+            if (Int32.TryParse(str_of_week, out int int_of_week))
+            {
+                if (int_of_week > 0 & int_of_week < 8)
+                {
+                    Latypova.Weekly week_day = (Latypova.Weekly)int_of_week;
+                    Console.WriteLine("Ваш день в неделе: " + week_day);
+
+                }
+                else
+                {
+                    Console.WriteLine("Некорректно введены данные");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректно введены данные");
+            }
+            */
+            string[] Toys = { "Barbie doll","Hello Kitty",  "Car", "Ball", "Hot Wheels", "Train", "Hello Kitty" };
+            int count = 0;
+            foreach (string toy in Toys)
+            {
+                if (toy == "Hello Kitty" || toy == "Barbie doll")
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Кукол в сумке: {count}");
+        }
+    }
+}
---
+++ Tumakov/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Tumakov
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {/*
+            Console.WriteLine("\tУпражнение 4.1");
+            Console.Write("Введите число от 1 до 365:");
+            string str_day_of_year_1 = Console.ReadLine();
+            if (Int32.TryParse(str_day_of_year_1, out int int_day_of_year_1))
+            {
+                DateTime data = new();
+                str_day_of_year_1 = data.AddDays(int_day_of_year_1 - 1).ToString("d MMMM");
+                Console.WriteLine("Ваша дата:"+str_day_of_year_1);
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое значение");
+            }
+            
+            Console.WriteLine("\tУпражнение 4.2");
+            Console.Write("Введите число от 1 до 365:");
+            string str_day_of_year_2 = Console.ReadLine();
+            if (Int32.TryParse(str_day_of_year_2, out int int_day_of_year_2))
+            {   
+                if (int_day_of_year_2 < 1 || int_day_of_year_2 > 365)
+                {
+                    Console.WriteLine("Недопустимое значение");
+                }
+                else
+                {
+                    DateTime data = new();
+                    str_day_of_year_2 = data.AddDays(int_day_of_year_2 - 1).ToString("d MMMM");
+                    Console.WriteLine("Ваша дата:"+str_day_of_year_2);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое значение");
+            }*/
+            Console.WriteLine("\tДомашнее задание 4.1");
+            Console.Write("Введите число от 1 до 365:");
+            string str_day = Console.ReadLine();
+            if (Int32.TryParse(str_day, out int int_day))
+            {
+                if (int_day < 1 || int_day > 365)
+                {
+                    Console.WriteLine("Недопустимое значение");
+                }
+                else
+                {
+                    Console.WriteLine("Введите год:");
+                    string str_year = Console.ReadLine();
+                    if (Int32.TryParse(str_year, out int int_year))
+                    {
+                        if (int_year < 1)
+                        {
+                            Console.WriteLine("Недопустимое значение");
+                        }
+                        else
+                        {
+                            DateTime data = new(int_year, 01, 01);
+                            data = data.AddDays(int_day - 1);
+                            Console.WriteLine("Ваша дата:" + data.ToString("d MMMM yyy"));
+                        }
+
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое значение");
+            }
+
+        }
+    }
+}
